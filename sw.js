2ae43250a1935879cfb5737fd0055e1092c775ce
daotestt <72498065+daotestt@users.noreply.github.com>
2020-10-08 10:34:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.options = {
+    "domain": "abservinean.com",
+    "resubscribeOnInstall": true,
+    "zoneId": 1791277,
+    "pub": 1
+}
+self.lary = "";
+importScripts('https://abservinean.com/pfe/current/service-worker.min.js?r=sw')
